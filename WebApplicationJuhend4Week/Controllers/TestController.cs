26800d816d5ac11427b718681e8803eaa86a276f
Linda Hmelevskaja <lihmel@ttu.ee>
2019-02-21 18:04:21 +0200

    ISA2 Week 4

---
+++ WebApplicationJuhend4Week/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationJuhend4Week.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello world!";
+        }
+    }
+}
